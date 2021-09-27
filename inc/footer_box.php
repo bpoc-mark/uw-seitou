7a20bf969f028fc67ff982d1dae6dc8172740c49
bpoc-mark <markariel.maata@gmail.com>
2021-09-27 19:40:16 +0800

    added footer pc and sp view

--- inc/footer_box.php
+++ inc/footer_box.php
@@ -1,18 +1,99 @@
 <div class="footer_inner">
-
-	<div class="logo"><a href="/"><img src="" alt="<?php echo SITE_NAME; ?>"></a></div>
-
-	<nav>
-		<ul>
-			<li><a href="/">ホーム</a></li>
-			<li><a href="/about/">このサイトついて</a></li>
-			<li><a href="/sitemap">サイトマップ</a></li>
-			<li><a href="/privacy">プライバシーポリシー</a></li>
-		</ul>
-	</nav>
-
-	<div class="copy">
-		<small>&copy; ウルトラワークス　All Lights Reserved.</small>
+	<div class="logo"><a href="/"><img src="/images/common/footer_logo.svg" alt="<?php echo SITE_NAME; ?>"></a></div>
+	<div class="row">
+		<div class="col">
+			<nav>
+				<ul>
+					<li>
+						<a href="/">TOP</a>
+					</li>
+					<li>
+						<a href="/">SHOP INFO</a>
+						<ul class="sub_menu">
+							<li>
+								<a href="">キッチングッズ</a>
+							</li>
+							<li>
+								<a href="">ギフトショップ</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="/"><span class="pc">FOOD BUSINESS</span> ONE STOP SERVICE</a>
+					</li>
+				</ul>
+				<ul>
+					<li>
+						<a href="/">COMPANY</a>
+						<ul class="sub_menu">
+							<li>
+								<a href="">ごあいさつ</a>
+							</li>
+							<li>
+								<a href="">事業内容</a>
+							</li>
+							<li>
+								<a href="">会社概要</a>
+							</li>
+							<li>
+								<a href="">沿革</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="/">RECRUIT</a>
+					</li>
+					<li>
+						<a href="/">PICK UP</a>
+					</li>
+				</ul>
+			</nav>
+			<p>
+				<a href="">CONTACT</a>
+				<a href="">PRIVACY POLICY</a>
+			</p>
+			<div class="copy pc">
+				<small>Copyright &copy; SEITOU All rights reserved</small>
+			</div>
+		</div>
+		<div class="col">
+			<ul class="img_link">
+				<li>
+					<a href="">
+						<img src="/images/common/img_1.png" alt="">
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<img src="/images/common/img_2.png" alt="">
+					</a>
+				</li>
+			</ul>
+			<ul class="sns_link">
+				<li>
+					<a href="javascript:void(0)">Follow me</a>
+				</li>
+				<li>
+					<a href=""><img src="/images/common/line_icon.svg" alt=""> <span>SEITOU<br>OFFICIAL LINE</span></a>
+				</li>
+				<li>
+					<a href=""><img src="/images/common/ig_icon.svg" alt=""> <span>KITCHEN<br>STUDIO SEI</span></a>
+				</li>
+				<li>
+					<a href=""><img src="/images/common/ig_icon.svg" alt=""> <span>GIFT SHOP</span></a>
+				</li>
+			</ul>
+			<div class="footer_details">
+				<img src="/images/common/footer_logo1.svg.png" alt="">
+				<p>
+				株式会社セイトウ <br>
+				〒438-0071 静岡県磐田市今之浦1-7-6 <br>
+				Tel.<a href="tel:0538-34-2191">0538-34-2191</a> ／ Fax.<a href="tel:0538-34-2195">0538-34-2195</a>
+				</p>
+			</div>
+			<div class="copy sp">
+				<small>Copyright &copy; SEITOU All rights reserved</small>
+			</div>
+		</div>
 	</div>
-
 </div>
