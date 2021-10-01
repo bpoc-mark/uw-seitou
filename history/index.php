37d5694f30979d86972515cecd177a9cd6d6e617
bpoc-mark <markariel.maata@gmail.com>
2021-10-01 14:00:07 +0800

    added history page pc and sp view

---
+++ history/index.php
@@ -0,0 +1,368 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/config.php"); ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/tag_manager.php"); ?>
+  <meta charset="UTF-8" />
+  <meta name="robots" content="index,follow">
+  <link rel="canonical" href="<?php echo CANONICAL ?>">
+  <meta name="keywords" content="<?php echo KEY_WORD ?>">
+  <meta name="description" content="<?php echo DESCRIPTION ?>">
+  <meta property="og:title" content="<?php echo SITE_NAME ?>">
+  <meta property="og:type" content="article" /> <!-- 下層では「article」にしてください -->
+  <meta property="og:description" content="<?php echo DESCRIPTION ?>" />
+  <meta property="og:url" content="<?php echo CANONICAL ?>" />
+
+  <title>沿 革 | <?php echo SITE_NAME ?></title>
+
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_include.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/ga.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/header_tag.php"); ?>
+</head>
+
+<body id="History">
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/content_tag.php"); ?>
+  <div id="AllBox">
+    <header>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_box.php"); ?>
+    </header>
+
+    <main>
+      <section class="jumbo_sect">
+        <div class="wrapper">
+          <div class="breadcrumbs">
+            <ul class="">
+              <li>
+                <a href="/history/">HISTORY</a>
+              </li>
+              <li>
+                <a href="/history/">沿 革</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section class="sect_1">
+        <div class="wrapper">
+          <ul>
+            <li>
+              <a href="/greetings/" <?php if ($str[1] == "greetings") {
+												echo ' class="here"';
+											} ?>>ごあいさつ</a>
+            </li>
+            <li>
+              <a href="/business/" <?php if ($str[1] == "business") {
+												echo ' class="here"';
+											} ?>>事業内容</a>
+            </li>
+            <li>
+              <a href="/company/" <?php if ($str[1] == "company") {
+												echo ' class="here"';
+											} ?>>会社概要</a>
+            </li>
+            <li>
+              <a href="/history/" <?php if ($str[1] == "history") {
+												echo ' class="here"';
+											} ?>>沿  革</a>
+            </li>
+          </ul>
+          <div class="row">
+            <div class="col">
+              <h4>PHILOSOPHY <span>/  経営理念</span></h4>
+              <p>私たちは、笑顔あふれる彩り豊かな空間創りで社会に貢献します。</p>
+            </div>
+            <div class="col">
+              <h4>THREE PRINCIPLES <span>/  三原則</span></h4>
+              <p>一、時を守る。<span class="pc">　</span><br class="sp">二、場を浄める。<span class="pc">　</span><br class="sp">三、礼を正す。</p>
+            </div>
+          </div>
+          <div class="history_list">
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    1973年
+                  </td>
+                  <td>
+                    磐田郡豊田町にて個人創業。静岡県西部地方スーパーを柱に販売開始。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1976年
+                  </td>
+                  <td>
+                  業務拡大により磐田市国府台に移転｡県内生協と取引開始。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1978年
+                  </td>
+                  <td>
+                  販売エリア全県下に拡大。生協取引県外にも拡大。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1981年
+                  </td>
+                  <td>
+                  株式会社セイトウを設立。資本金1,000万円。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1982年
+                  </td>
+                  <td>
+                  磐田郡竜洋町に配送センター完成。業務用食器部門設立に際し本社内ショールーム開設。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1983年
+                  </td>
+                  <td>
+                  業務用食器浜松市内中心に販売開始。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1986年
+                  </td>
+                  <td>
+                  事務量増大に対応コンピュータ入替。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1987年
+                  </td>
+                  <td>
+                  生協部門の物流増大に対して浜名梱包輸送(株)と物流面の提携。福利厚生の一環として浜名湖の保養所購入。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1989年
+                  </td>
+                  <td>
+                  社内、分社制の導入。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1991年
+                  </td>
+                  <td>
+                  陶芸教室、手作り品の(株)アートセラミック設立。専門講師による社内研修開始。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1992年
+                  </td>
+                  <td>
+                  ギフトプレミアム部門をエス･ジー･シー(株)として分社。通販部門設立。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1993年
+                  </td>
+                  <td>
+                  （有）アートラッピング（物流センター）を下石田に開設、倉庫の統合を行う。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1994年
+                  </td>
+                  <td>
+                    磐田市今之浦に移転。業務用ショールーム「キッチンスタジアム」、ギフトショップ「ギフトピュア」を開設。資本金4,000万円に増資。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1995年
+                  </td>
+                  <td>
+                  ギフトピュア県西部の式場とタイアップ販売開始。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1996年
+                  </td>
+                  <td>
+                  浜松市笠井町遠鉄ショッピングタウンリブロス笠井内に雑貨ショップ「きっちんぴゅあ」オープン。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1997年
+                  </td>
+                  <td>
+                  部門統合により通販部門を2部門へ統合。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1998年
+                  </td>
+                  <td>
+                  創業25周年。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    1999年
+                  </td>
+                  <td>
+                  調理用品・道具のキッチンスタジアム開店5周年リニューアルオープン。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    2000年
+                  </td>
+                  <td>
+                  売筋の変化に合わせ「和雑貨」の本格販売の開始。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    2001年
+                  </td>
+                  <td>
+                  直営ショップのギフトピュアとキッチンピュアを統合し㈱ピュアを設立する。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    2003年
+                  </td>
+                  <td>
+                  経営合理化のためSGC㈱の営業を㈱セイトウが引き継ぐ。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    2004年
+                  </td>
+                  <td>
+                  創業30周年。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    2006年
+                  </td>
+                  <td>
+                  業務拡張に伴い隣地535㎡購入。資本金5,200万円に増資。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    2009年
+                  </td>
+                  <td>
+                  物流部門を有限会社アートラッピングよりセイトウ磐田物流センターへ移行。ISO9001認証取得。経営革新企業（静岡県）に認証。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    2010年
+                  </td>
+                  <td>
+                  経営合理化のため㈱ピュアの営業を㈱セイトウが引き継ぎ、小売事業部とする。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    2011年
+                  </td>
+                  <td>
+                  ショールーム建設のため駐車場として、隣地450㎡購入。テレビショッピングとの本格的取組み開始。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    2011年
+                  </td>
+                  <td>
+                  隣地に床面積100坪、3階建のショールーム完成
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    2012年
+                  </td>
+                  <td>
+                  FHAシンガポール食品展示会に初出展。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    2013年
+                  </td>
+                  <td>
+                  創業40周年。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    2014年
+                  </td>
+                  <td>
+                  磐田市見付プラザ21 遠鉄ストア見付店内に雑貨ショップ｢きっちんぴゅあ見付店｣オープン。
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    2017年
+                  </td>
+                  <td>
+                  東洋厨機工業株式会社　株式取得
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    2021年
+                  </td>
+                  <td>
+                  東洋厨機工業株式会社　株式取得
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
+      <section class="footer_breadcrumbs pc">
+        <div class="wrapper">
+          <ul>
+            <li>
+              <a href="/">TOP</a>
+            </li>
+            <li>
+              <a href="/company/">会社案内</a>
+            </li>
+            <li>
+              <a href="/history/">沿 革</a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
+
+    <footer>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/footer_box.php"); ?>
+    </footer>
+  </div><!-- //AllBox -->
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/footer_tag.php"); ?>
+</body>
+
+</html>
