10121a58ad4b58c23935e117ea83b54465d02d05
bpoc-mark <markariel.maata@gmail.com>
2021-10-05 17:45:53 +0800

    added one stop service page pc and sp view

---
+++ service/index.php
@@ -0,0 +1,395 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/config.php"); ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/tag_manager.php"); ?>
+  <meta charset="UTF-8" />
+  <meta name="robots" content="index,follow">
+  <link rel="canonical" href="<?php echo CANONICAL ?>">
+  <meta name="keywords" content="<?php echo KEY_WORD ?>">
+  <meta name="description" content="<?php echo DESCRIPTION ?>">
+  <meta property="og:title" content="<?php echo SITE_NAME ?>">
+  <meta property="og:type" content="article" /> <!-- 下層では「article」にしてください -->
+  <meta property="og:description" content="<?php echo DESCRIPTION ?>" />
+  <meta property="og:url" content="<?php echo CANONICAL ?>" />
+
+  <title>ワンストップサービス | <?php echo SITE_NAME ?></title>
+
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_include.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/ga.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/header_tag.php"); ?>
+</head>
+
+<body id="Service">
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/content_tag.php"); ?>
+  <div id="AllBox">
+    <header>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_box.php"); ?>
+    </header>
+
+    <main>
+      <section class="jumbo_sect">
+        <div class="wrapper">
+          <div class="breadcrumbs">
+            <ul class="">
+              <li>
+                <a href="/coop/">ONE STOP SERVICE</a>
+              </li>
+              <li>
+                <a href="/coop/">ワンストップサービス</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section class="sect_1">
+        <div class="wrapper">
+          <div class="row">
+            <div class="col">
+              <h3>「料理」を最大限に<br class="sp">美味しく見せる<br class="pc">
+                  遊び心ある<br class="sp">食器コーディネート</h3>
+            </div>
+            <div class="col">
+              <p>
+                株式会社セイトウは、1973年の創業。以来、飲食ビジネスに求められる<br>
+                ありとあらゆるモノを取扱う総合商社として、ご愛顧をいただいてまいりました。<br>
+                モノをリーズナブルにご提供するにとどまらず、<br>
+                「こんな店にしたい」、「こんなものが欲しい」、「うちの店にぴったり合うものは?」、<br>
+                「こんな予算で考えているんだが」など、お客様のご要望にお応えし、<br>
+                イメージを具現化するご支援は、私たちの大切な役割となっています。<br>
+                セイトウではお客様のこのようなご要望をしっかりと把握してプランを組み立て、<br>
+                数多の内外の信頼のおけるブランドからコーディネイトをいたします。<br>
+                お客様のイメージに合った最高クラスのものをトータルでご提案し、<br>
+                ご予算内で収めるために、長年の経験値と培った提案力で、<br>
+                セイトウは全力でお客様のサポートをお約束いたします。
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="sect_2">
+        <div class="wrapper">
+          <div class="head_cont">
+            <h3>WORKS</h3>
+            <a href="">VIEW ALL</a>
+          </div>
+          <div class="row">
+            <div class="col">
+              <a href="">
+                <img src="/images/service/row_2_img1.png" alt="">
+                <div class="post_data">
+                  <p class="post_date_cat">
+                    <span>2021.05.01</span>
+                    <span>レストラン</span>
+                  </p>
+                  <p class="post_title">浜北区中瀬 こんがりパン工房ペパル様</p>
+                </div>
+              </a>
+            </div>
+            <div class="col">
+              <a href="">
+                <img src="/images/service/row_2_img2.png" alt="">
+                <div class="post_data">
+                  <p class="post_date_cat">
+                    <span>2021.05.01</span>
+                    <span>Cafe</span>
+                  </p>
+                  <p class="post_title">浜北区中瀬 こんがりパン工房ペパル様</p>
+                </div>
+              </a>
+            </div>
+            <div class="col">
+              <a href="">
+                <img src="/images/service/row_2_img3.png" alt="">
+                <div class="post_data">
+                  <p class="post_date_cat">
+                    <span>2021.05.01</span>
+                    <span>和食</span>
+                  </p>
+                  <p class="post_title">浜北区中瀬 こんがりパン工房ペパル様</p>
+                </div>
+              </a>
+            </div>
+            <div class="col">
+              <a href="">
+                <img src="/images/service/row_2_img1.png" alt="">
+                <div class="post_data">
+                  <p class="post_date_cat">
+                    <span>2021.05.01</span>
+                    <span>レストラン</span>
+                  </p>
+                  <p class="post_title">浜北区中瀬 こんがりパン工房ペパル様</p>
+                </div>
+              </a>
+            </div>
+            <div class="col">
+              <a href="">
+                <img src="/images/service/row_2_img2.png" alt="">
+                <div class="post_data">
+                  <p class="post_date_cat">
+                    <span>2021.05.01</span>
+                    <span>Cafe</span>
+                  </p>
+                  <p class="post_title">浜北区中瀬 こんがりパン工房ペパル様</p>
+                </div>
+              </a>
+            </div>
+            <div class="col">
+              <a href="">
+                <img src="/images/service/row_2_img3.png" alt="">
+                <div class="post_data">
+                  <p class="post_date_cat">
+                    <span>2021.05.01</span>
+                    <span>和食</span>
+                  </p>
+                  <p class="post_title">浜北区中瀬 こんがりパン工房ペパル様</p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="sect_3">
+        <div class="wrapper">
+          <h3>POINT</h3>
+          <div class="point">
+            <div class="row">
+              <div class="col">
+                <p>01</p>
+              </div>
+              <div class="col">
+                <ul>
+                  <li>
+                    <img src="/images/service/row_3_img1.png" alt="">
+                  </li>
+                  <li>
+                    <img src="/images/service/row_3_img2.png" alt="">
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="info">
+              <h4>展示品約25,000点！ショールームの圧倒的品揃え</h4>
+              <p>セイトウの業務用ショールーム [キッチンスタジアム] は県下最大規模、約25,000点に及ぶ最新の飲食ビジネスに関わるあらゆるアイテムを取り揃えております。プロの料理道具は言うに及ばず、 国産や海外一流ブランドの皿などの陶磁器、 ガラス食器、 カトラリーといったテーブルウェアから椅子・テーブル、 什器類、 さらには、ラップやホイル·洗剤などの消耗品、 メニューやのれん・のぼりなどの販促用品、POSレジスターに至るまで、すべてワンストップで揃えていただくことができます。</p>
+            </div>
+          </div>
+          <div class="point">
+            <div class="row">
+              <div class="col">
+                <p>02</p>
+              </div>
+              <div class="col">
+                <ul>
+                  <li>
+                    <img src="/images/service/row_3_img3.png" alt="">
+                  </li>
+                  <li>
+                    <img src="/images/service/row_3_img4.png" alt="">
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="info">
+              <h4>プロの目利き御用達</h4>
+              <p>
+                セイトウのショールームでは、見て、触れて、確かめていただくのが基本。カタログだけでは分かり難い、サイズや重量感、手触りや質感などを実際に確かめることができます。ご来場いただければ飲食店、ホテル、給食施設など、その道のプロのお客様が時間をかけ、納得いくまで品定めされている様子を見ることができます。一方、これから業界に一歩踏みだされるビギナーのお客様には、親身にご相談に乗り、セイトウの提案力をフルに発揮。<br>
+                グループのワンストップサービス支援を惜しみません！なんでも遠慮なくご相談ください。
+              </p>
+            </div>
+          </div>
+          <div class="point">
+            <div class="row">
+              <div class="col">
+                <p>03</p>
+              </div>
+              <div class="col">
+                <ul>
+                  <li>
+                    <img src="/images/service/row_3_img5.png" alt="">
+                  </li>
+                  <li>
+                    <img src="/images/service/row_3_img6.png" alt="">
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="info">
+              <h4>セイトウの、地元密着バックアップシステム</h4>
+              <p>
+                セイトウにはお客様に定期的に訪問する「セイトウバックアップチーム」があります。<br>
+                ご注文を頂いた商品のお届けだけではなく、ラップ、ペーパータオル、洗剤やペーパーナプキン、つまようじ等の消耗品の補充を行っています。また、訪問の際には新製品のパンフレットなどの情報提供をはじめ、お客様の様々なご要望を聞き取っています。きめ細かいフォローは、地域密着型のセイトウならではのサービスです。
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="sect_4">
+        <div class="wrapper">
+          <h3>GROUP COMPANY</h3>
+          <div class="row">
+            <div class="col">
+              <h4>
+                事業計画のご相談から <br>
+                メンテナンスまで。
+              </h4>
+              <p>トータルプロデュースの底力</p>
+            </div>
+            <div class="col">
+              <p>
+                グループ企業である、「東洋厨機工業」と連携し、<br>
+                厨房機器のご提案から修理サポートまで対応させていただきます。<br>
+                納入実績3,000件超の実力！食文化ビジネスを支えて半世紀。<br>
+                飲食ビジネスのベテランからビギナーにまで、<br>
+                東洋厨機工業のトータルプロデュースは開業・改装のお役に立ってまいりました。<br>
+                和洋中の飲食店をはじめ、レストラン、病院、給食センター、福祉施設、ホテル、<br>
+                ゴルフ場、カフェ、居酒屋から移動式店舗まで、あらゆる業態のあらゆるシーンで<br>
+                ワンストップの〈100%プロデュース〉が可能です。
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="sect_5">
+        <div class="wrapper">
+          <h3>POINT</h3>
+          <div class="point_cont">
+            <div class="row">
+              <div class="col">
+                <span>01</span>
+                <h5>
+                  食ビジネスの入門者からベテランまで、<br>
+                  事業計画への助言・調査や土地・<br class="sp">物件探しのお手伝い
+                </h5>
+                <h4>他社をリードする「強み」、<br class="sp">東洋厨機工業の「お客様支援」</h4>
+                <p>
+                  新規開店・リニューアルなど、お客さまのビジネス展開計画へのアドバイスや調査のお手伝いをはじめ、土地・建物の物件探しから、食材・おしぼり業者の手配といったことまで、お役に立つための広いネットワークを組んでご相談を承っています。<br>
+                  こうした適切な情報提供やアドバイスは、起こりがちなビギナーのひとりよがりを防ぐことにもなり、東洋厨機工業への信頼の源泉となっています。これら「お客さま支援」は、リース・金融といった大切な開業資金計画のご相談·アドバイスとともに、東洋厨機工業の企業文化ともなって根づいています。第一歩は、まず私たちに気軽にご相談いただくことからお始めください。
+                </p>
+              </div>
+              <div class="col">
+                <picture>
+                  <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                  <source srcset="/images/service/sp/row_5_img1.png" media="(max-width: 899px)" type="image/webp">
+                  <source srcset="/images/service/row_5_img1.png" media="(min-width: 900px)">
+                  <img src="/images/service/sp/row_5_img1.png" alt="">
+                </picture>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <span>02</span>
+                <h5>
+                  時代を見据えた企画・提案 <br>
+                  どこまでも厨房で働く方の目線に立っての機器選び
+                </h5>
+                <h4>3,000件超の納入実績と豊かなノウハウ、提案力</h4>
+                <p>
+                  お客様の夢をカタチにしていきます。ご希望やイメージを詳しくお聞かせください。<br>
+                  厨房は多様な業種、業態、規模、調理形態などによって異り、オンリーワンの厨房開発が珍しくありません。東洋厨機工業では最新の設備機器の選定※1 や情報の提供に加え、3,000件超の納入実績のノウハウ※2 をもってお客さまと共にプランを組上げ、ラフスケッチなどで具体化していきます。私たちの培ってきた提案力が試される好機だと思っています。
+                </p>
+                <div class="list">
+                  <p>
+                    ※1…飲食店の内装工事は、他業種と較べて給排水やダクト工事、厨房設備工事への費用割合が大きくなります。この負担を軽減するために、東洋厨機工業では、お客さまに併設の「リカバリーたっちゃん」の大展示場で、実際に新古品・再生中古品をご覧になっていただき、これを要所要所に上手にお使いになることをおすすめしています。
+                  </p>
+                  <p>
+                    ※2…多数のメーカーの製品を取り扱ってきたことでその製品の「良し悪し」を熟知しており、特定メーカーに偏らない公正中立なご提案 ・「おすすめ」ができる点が、東洋厨機工業の特長です。
+                  </p>
+                </div>
+              </div>
+              <div class="col">
+                <picture>
+                  <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                  <source srcset="/images/service/sp/row_5_img2.png" media="(max-width: 899px)" type="image/webp">
+                  <source srcset="/images/service/row_5_img2.png" media="(min-width: 900px)">
+                  <img src="/images/service/sp/row_5_img2.png" alt="">
+                </picture>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <span>03</span>
+                <h5>
+                  厨房の複雑な諸機能を、効果的にとり<br class="sp">まとめるスペシャリスト <br>
+                  フード、ダクト、ステンレス張り分野では<br class="sp">圧倒的な、精度と仕上りを誇ります
+                </h5>
+                <h4>機能性と安全性を追求する設計・施工</h4>
+                <p>
+                  厨房には、電気やガス、給排水設備などの諸機能が複雑に集中し、そしてまた、食品衛生面での格別の配慮を必要とする空間でもあります。<br>
+                  これらに加え、動線やゾーニング、換気・空調といったさまざまな条件をクリアする必要があり、私たち設計部門の腕の見せどころとなります。<br>
+                  施工は専門の作業スタッフにより、品質と安全が最優先の工程のもと進められます。
+                </p>
+              </div>
+              <div class="col">
+                <picture>
+                  <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                  <source srcset="/images/service/sp/row_5_img3.png" media="(max-width: 899px)" type="image/webp">
+                  <source srcset="/images/service/row_5_img3.png" media="(min-width: 900px)">
+                  <img src="/images/service/sp/row_5_img3.png" alt="">
+                </picture>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <span>04</span>
+                <h5>
+                  不測のトラブルにはサービス班が急行 <br>
+                  年間通しての安心態勢
+                </h5>
+                <h4>心強い、要修理時の即応力</h4>
+                <p>
+                  厨房設備·機器の機能部品や消耗部品などの、劣化·故障は避けて通ることができません。東洋厨機工業では、これらの問題に対しては導入時のていねいな取扱説明や、 消耗部品の定期的な交換によって突発的な故障を未然に防ぐよう努めています。このことが故障によるニ次的被害や営業上の損失を防ぐ最善の方法だからです。また、残念ながらそれでも起こり得る突然のトラブルには、“休めない厨房を休ませない”ためのサービス班が急行。厨房設備施工技能士、冷媒フロン類取得技術者などベテラン有資格者が対応いたします。<br>
+                  メーカー等では時として難しいこともある即時対応。地元密着ならではの即応力と、蓄積された技術力は東洋厨機工業の強みでもあります。
+                </p>
+              </div>
+              <div class="col">
+                <picture>
+                  <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                  <source srcset="/images/service/sp/row_5_img4.png" media="(max-width: 899px)" type="image/webp">
+                  <source srcset="/images/service/row_5_img4.png" media="(min-width: 900px)">
+                  <img src="/images/service/sp/row_5_img4.png" alt="">
+                </picture>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="sect_6">
+        <div class="wrapper">
+          <h3>CONTACT</h3>
+          <ul>
+            <li>
+              <a href="">MAIL FORM 
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 33.56 27.38"><defs><style>.cls-1,.cls-2{fill:none;stroke:#21201f;stroke-width:1.77px;}.cls-2{stroke-linecap:round;}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="レイヤー_1" data-name="レイヤー 1"><polyline class="cls-1" points="32.68 6.18 32.68 26.5 0.88 26.5 0.88 0.88 32.68 0.88 32.68 6.18"/><polyline class="cls-2" points="6.18 9.72 16.78 16.78 27.38 9.72"/></g></g></svg>
+              </a>
+            </li>
+            <li>
+              <a href="tel:0538-34-2191">Tel. 0538-34-2191 
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 33.95 33.94"><defs><style>.cls-1,.cls-2{fill:none;stroke:#21201f;stroke-linecap:round;stroke-width:2px;}.cls-1{stroke-miterlimit:10;}.cls-2{stroke-linejoin:round;}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="レイヤー_1" data-name="レイヤー 1"><path class="cls-1" d="M32,1c.93.17,1,1.42.93,2.12C32,20,19,32,3,32.94c-.65,0-2-.24-2-.94"/><path class="cls-1" d="M19.36,19.41a26.35,26.35,0,0,1-5.64,4.27"/><polyline class="cls-2" points="1 22 9.99 19.98 13.72 23.68"/><polyline class="cls-2" points="22 1 19.98 9.99 23.64 13.78"/><line class="cls-1" x1="32" y1="1" x2="22" y2="1"/><line class="cls-1" x1="1" y1="22" x2="1" y2="32"/></g></g></svg>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section class="footer_breadcrumbs pc">
+        <div class="wrapper">
+          <ul>
+            <li>
+              <a href="/">TOP</a>
+            </li>
+            <li>
+              <a href="/service/">ワンストップサービス</a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
+
+    <footer>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/footer_box.php"); ?>
+    </footer>
+  </div><!-- //AllBox -->
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/footer_tag.php"); ?>
+</body>
+
+</html>
