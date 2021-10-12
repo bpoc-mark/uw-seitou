3644f0f2ef5ca46d3ecb62c023b60e44cbac3dab
bpoc-mark <markariel.maata@gmail.com>
2021-10-12 11:52:23 +0800

    added recruit page pc and sp view

---
+++ recruit/index.php
@@ -0,0 +1,275 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/config.php"); ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/tag_manager.php"); ?>
+  <meta charset="UTF-8" />
+  <meta name="robots" content="index,follow">
+  <link rel="canonical" href="<?php echo CANONICAL ?>">
+  <meta name="keywords" content="<?php echo KEY_WORD ?>">
+  <meta name="description" content="<?php echo DESCRIPTION ?>">
+  <meta property="og:title" content="<?php echo SITE_NAME ?>">
+  <meta property="og:type" content="article" /> <!-- 下層では「article」にしてください -->
+  <meta property="og:description" content="<?php echo DESCRIPTION ?>" />
+  <meta property="og:url" content="<?php echo CANONICAL ?>" />
+
+  <title>採用情報 | <?php echo SITE_NAME ?></title>
+
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_include.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/ga.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/header_tag.php"); ?>
+</head>
+
+<body id="Recruit">
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/content_tag.php"); ?>
+  <div id="AllBox">
+    <header>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_box.php"); ?>
+    </header>
+
+    <main>
+      <section class="jumbo_sect">
+        <div class="wrapper">
+          <div class="breadcrumbs">
+            <ul class="">
+              <li>
+                <a href="/recruit/">RECRUIT</a>
+              </li>
+              <li>
+                <a href="/recruit/">採用情報</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section class="sect_1">
+        <div class="wrapper">
+          <div class="row">
+            <div class="col">
+              <h3 class="c-anim-up">
+                語れる夢なんか今はなくても、<br>
+                ここに来れば<br class="sp">語れるようになる
+              </h3>
+            </div>
+            <div class="col">
+              <p class="c-anim-up">
+                「あなたの夢は何ですか?」<br>
+                これから新社会人になる君たちにとって何度も何度も聞かれた質問だろう。<br>
+                君は本当に夢を持っているか？就職活動用の答えじゃないのか？<br>
+                それどころか自分の才能は何か。自分が本当になりたいものは何か。<br>
+                分からないことだって多いはずだ。でも大丈夫。<br>
+                セイトウならきっと君の居場所ができる。そして誰もが成長できる。<br>
+                夢を大いに語り合える大人に。
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="sect_2">
+        <div class="wrapper">
+          <h3 class="c-anim-up">INTERVIEW</h3>
+          <div class="item">
+            <div class="top row">
+              <div class="col c-anim-up">
+                <span>01</span>
+              </div>
+              <div class="col c-anim-up">
+                <img src="/images/recruit/row_1_img1.png" alt="">
+              </div>
+              <div class="col c-anim-up">
+                <h4>一般事務</h4>
+              </div>
+            </div>
+            <div class="bottom row">
+              <div class="col pc c-anim-up">
+                <img src="/images/recruit/row_1_img2.png" alt="">
+              </div>
+              <div class="col c-anim-up">
+                <h5>「ありがとう」の一言がやりがいに。</h5>
+                <p>
+                  <img src="/images/recruit/row_1_img2.png" alt="" class="sp">
+                  高校時代、担任の先生に強く勤められ、採用試験を受けました。ギフトや器について、あまり詳しく知りませんでしたが、入社してから知識を身につけられる環境が整っています。<br>
+                  電話注文のお客様との何気ない会話も楽しく、「ありがとう」の言葉にやりがいを感じています。社内の雰囲気も大好きで、皆優しく居心地が良いのが自慢です。また、定時に仕事を終え、プライベートの時間を大切に過ごせる事も魅力のひとつだと思います。
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div class="top row">
+              <div class="col c-anim-up">
+                <span>02</span>
+              </div>
+              <div class="col c-anim-up">
+                <img src="/images/recruit/row_2_img1.png" alt="">
+              </div>
+              <div class="col c-anim-up">
+                <h4>
+                  業務用＆ <br>
+                  ブライダルギフト事業部業務用営業
+                </h4>
+              </div>
+            </div>
+            <div class="bottom row">
+              <div class="col pc c-anim-up">
+                <img src="/images/recruit/row_2_img2.png" alt="">
+              </div>
+              <div class="col c-anim-up">
+                <h5>更なるキャリアアップを目指して</h5>
+                <p>
+                  <img src="/images/recruit/row_2_img2.png" alt="" class="sp">
+                  外食好きが高じ、器に興味を持ったのがきっかけです。もともと人見知りで、苦手を克服したく営業職を希望しました。現在は飲食店や福祉施設などへ食器や厨房の調理備品などの営業販売をしています。飲食のプロと対等に会話ができる様、流行のデザインや料理に合う器を勉強するようになりました。会話をすることで、よりお客様ともコミュニケーションが取れ、人見知りも克服できたと思います。更にキャリアアップを目指し、フィールドを広げていきたいです。
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div class="top row">
+              <div class="col c-anim-up">
+                <span>03</span>
+              </div>
+              <div class="col c-anim-up">
+                <img src="/images/recruit/row_3_img1.png" alt="">
+              </div>
+              <div class="col c-anim-up">
+                <h4>ギフトピュア店長</h4>
+              </div>
+            </div>
+            <div class="bottom row">
+              <div class="col pc c-anim-up">
+                <img src="/images/recruit/row_3_img2.png" alt="">
+              </div>
+              <div class="col c-anim-up">
+                <h5>知識を自然と学んでゆける環境</h5>
+                <p>
+                  <img src="/images/recruit/row_3_img2.png" alt="" class="sp">
+                  家が近く通勤に便利かなと思い、就職説明会に参加し入社しました。働きながら自然と、ギフトや食器の知識が身に付き、安心して働くことが出来ました。人と会話することも苦手でしたが、お客様の役に立つことに徐々に喜びを感じ、今では店舗運営の全てを任されています。<br>
+                  お店と共に自分自身の成長も感じ、上司の勧めで販売士の資格も取得しました。地域に根付き、いつまでも愛されるショップ作りを目指したいと思います。
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="sect_3">
+        <div class="wrapper">
+          <div class="row">
+            <div class="col c-anim-up">
+              <h3>
+              あなたらしく成長し <br>
+              楽しいと思える仕事に<br class="sp">出会える
+              </h3>
+            </div>
+            <div class="col c-anim-up">
+              <p>
+                良い意味で包み隠さず本音が言え、居場所がある、それがセイトウです。<br>
+                どの社員も採用後、働きながら自分自身と向き合い、<br>
+                自然と夢や目標を持って走り続けています。<br>
+                セイトウではあなたが輝くステージを共に探してゆきます。
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="sect_4">
+        <div class="wrapper">
+          <h3 class="c-anim-up">TYPE OF WORK</h3>
+          <div class="list_cont row">
+            <div class="col c-anim-up">
+              <img src="/images/recruit/img_1.png" alt="">
+              <h4>ホテル飲食店向け営業</h4>
+              <h3>希望に寄り添い、提案する</h3>
+              <p>
+                各飲食店やチェーン店などへ出向き、食器や厨房の調理備品等の販売を行います。店舗や企業にとって、何が1番良いかを共に考え相談し、提案する仕事です。
+              </p>
+            </div>
+            <div class="col c-anim-up">
+              <img src="/images/recruit/img_2.png" alt="">
+              <h4>カタログ・通販営業</h4>
+              <h3>新商品を生み出すエキスパート</h3>
+              <p>
+                各種展示会で新商品を発掘したり、バイヤーと商談し商品提案を行います。ヒット商品を自分たちの企画で作り出せます。
+              </p>
+            </div>
+            <div class="col c-anim-up">
+              <img src="/images/recruit/img_3.png" alt="">
+              <h4>店舗スタッフ</h4>
+              <h3>店舗運営全てを担うスペシャリスト</h3>
+              <p>
+                お客様への接客や、ギフトの包装、季節に合わせたイベントの企画などの店舗に関わる業務全般を行います。また、商品のディスプレイは腕の見せ所です。
+              </p>
+            </div>
+            <div class="col c-anim-up">
+              <img src="/images/recruit/img_4.png" alt="">
+              <h4>店舗外商営業</h4>
+              <h3>信頼とコミュニケーションを築く</h3>
+              <p>
+              企業や銀行など直接出向いて、贈答用ギフトや販売促進ツールを提案し、販売します。トレンドや流行へのアンテナの高さも必要な仕事です。
+              </p>
+            </div>
+            <div class="col c-anim-up">
+              <img src="/images/recruit/img_5.png" alt="">
+              <h4>企画事務</h4>
+              <h3>使うシーンをわかりやすくデザイン</h3>
+              <p>
+              カタログやパンフレットに掲載するレイアウト、写真の撮り方、商品スペックの情報収集等を行います。またデータの分析を行い、製作してゆきます。
+              </p>
+            </div>
+            <div class="col c-anim-up">
+              <img src="/images/recruit/img_6.png" alt="">
+              <h4>物流業務</h4>
+              <h3>丁寧かつスピーディな応対</h3>
+              <p>
+              通販等お客様から注文を受け、商品を発送します。商品がちゃんと届くまでが私たちの仕事、真心を込めての応対が求められます。
+              </p>
+            </div>
+            <div class="col c-anim-up">
+              <img src="/images/recruit/img_7.png" alt="">
+              <h4>一般事務</h4>
+              <h3>他部署との連携をスムーズに</h3>
+              <p>
+              電話やメール応対、資料や契約書といった各種作成を行います。社内でのあらゆる事務作業を、共に働くメンバーが円滑に進められる様にサポートします。
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="sect_5">
+        <div class="wrapper">
+          <h3 class="c-anim-up">ENTRY & CONTACT</h3>
+          <ul>
+            <li class="c-anim-up">
+              <a href="/contact/">MAIL FORM 
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 33.56 27.38"><defs><style>.cls-1,.cls-2{fill:none;stroke:#21201f;stroke-width:1.77px;}.cls-2{stroke-linecap:round;}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="レイヤー_1" data-name="レイヤー 1"><polyline class="cls-1" points="32.68 6.18 32.68 26.5 0.88 26.5 0.88 0.88 32.68 0.88 32.68 6.18"/><polyline class="cls-2" points="6.18 9.72 16.78 16.78 27.38 9.72"/></g></g></svg>
+              </a>
+            </li>
+            <li class="c-anim-up">
+              <a href="tel:0538-34-2191">Tel. 0538-34-2191 
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 33.95 33.94"><defs><style>.cls-1,.cls-2{fill:none;stroke:#21201f;stroke-linecap:round;stroke-width:2px;}.cls-1{stroke-miterlimit:10;}.cls-2{stroke-linejoin:round;}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="レイヤー_1" data-name="レイヤー 1"><path class="cls-1" d="M32,1c.93.17,1,1.42.93,2.12C32,20,19,32,3,32.94c-.65,0-2-.24-2-.94"/><path class="cls-1" d="M19.36,19.41a26.35,26.35,0,0,1-5.64,4.27"/><polyline class="cls-2" points="1 22 9.99 19.98 13.72 23.68"/><polyline class="cls-2" points="22 1 19.98 9.99 23.64 13.78"/><line class="cls-1" x1="32" y1="1" x2="22" y2="1"/><line class="cls-1" x1="1" y1="22" x2="1" y2="32"/></g></g></svg>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section class="footer_breadcrumbs pc">
+        <div class="wrapper">
+          <ul>
+            <li>
+              <a href="/">TOP</a>
+            </li>
+            <li>
+              <a href="/recruit/">採用情報</a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
+
+    <footer>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/footer_box.php"); ?>
+    </footer>
+  </div><!-- //AllBox -->
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/footer_tag.php"); ?>
+</body>
+
+</html>
