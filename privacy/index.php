beb03e692ddea68da9b1206c1ba7e580d0b5bdfe
bpoc-mark <markariel.maata@gmail.com>
2021-10-13 13:25:57 +0800

    added privacy page pc and sp view / fix shop padding left error / replace contact form jumbo image

---
+++ privacy/index.php
@@ -0,0 +1,138 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/config.php"); ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/tag_manager.php"); ?>
+  <meta charset="UTF-8" />
+  <meta name="robots" content="index,follow">
+  <link rel="canonical" href="<?php echo CANONICAL ?>">
+  <meta name="keywords" content="<?php echo KEY_WORD ?>">
+  <meta name="description" content="<?php echo DESCRIPTION ?>">
+  <meta property="og:title" content="<?php echo SITE_NAME ?>">
+  <meta property="og:type" content="article" /> <!-- 下層では「article」にしてください -->
+  <meta property="og:description" content="<?php echo DESCRIPTION ?>" />
+  <meta property="og:url" content="<?php echo CANONICAL ?>" />
+
+  <title>プライバシーポリシー | <?php echo SITE_NAME ?></title>
+
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_include.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/ga.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/header_tag.php"); ?>
+</head>
+
+<body id="Privacy">
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/content_tag.php"); ?>
+  <div id="AllBox">
+    <header>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_box.php"); ?>
+    </header>
+
+    <main>
+      <section class="jumbo_sect">
+        <div class="wrapper">
+          <div class="breadcrumbs">
+            <ul class="">
+              <li>
+                <a href="/privacy/">PRIVACY POLICY</a>
+              </li>
+              <li>
+                <a href="/privacy/">プライバシーポリシー</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section class="sect_1">
+        <div class="wrapper">
+          <ul>
+            <li>
+              <p>
+                当社ウェブサイトのコンテンツのご利用にあたり、お客様個人を識別できる情報（以下、「個人情報」といいます）の提供をお願いする場合があります。<br>
+                その際はあらかじめその利用目的を明示し、お客様のご同意を得た上で適法かつ公正な手段で個人情報の収集を行なっています。<br>
+                お客様のご同意を得ずに個人情報を収集することはありません。
+              </p>
+            </li>
+            <li>
+              <h3>個人情報の利用目的</h3>
+              <p>
+                あらかじめご本人の同意を得ず、利用目的の達成に必要な範囲を超えて個人情報を取扱うことはありません。<br>
+                ご提供いただいた個人情報は、株式会社セイトウ（以下、当社といいます）が、お問い合せからのお客様へのご連絡の目的で利用するものとします。
+              </p>
+            </li>
+            <li>
+              <h3>個人情報の第三者への開示</h3>
+              <p>
+                当社では、個人情報を当社以外の第三者に提供する場合は、あらかじめお客様本人に同意を得て行います。<br>
+                ただし、法令に基づく場合や、人命、身体又は財産などの保護のために必要な場合は、この限りではありません。<br>
+                なお、以下の場合は、当該個人情報の提供を受ける者は第三者に該当しないものとします。
+              </p>
+              <p class="text-indent">・利用目的の達成に必要な範囲で、個人データの取扱いの全部もしくは一部を委託する場合。</p>
+              <p class="text-indent">・合併その他の事由による事業の承継に伴って個人データが提供される場合。</p>
+              <p class="text-indent">・個人データを共同利用する場合であって、あらかじめ共同して利用される個人データの項目、共同して利用する者の範囲、利用する者の利用目的及び当該個人データ管理責任者の氏名または名称を、本人に通知し、又は本人が容易に知り得る状態に置いている場合。</p>
+            </li>
+            <li>
+              <h3>登録情報の確認と変更</h3>
+              <p>
+                当社では、個人データについて、利用目的の達成に必要な範囲において、正確かつ最新の内容に保つよう努めます。<br>
+                お客様本人より当該本人の保有個人データの訂正、追加又は削除（「訂正等」という）を求められた場合、遅滞なく調査を行い、その結果に基づいて必要な訂正等を行います。
+              </p>
+            </li>
+            <li>
+              <h3>個人情報の安全管理措置</h3>
+              <p>
+                当社では、個人データの漏えい、滅失又はき損の防止その他の個人データの安全管理のために、必要な安全対策を講じます。
+              </p>
+            </li>
+            <li>
+              <h3>従業者の監督について</h3>
+              <p>
+                当社では、個人データを取扱う従業者に対し、当該個人データの安全管理が図られるよう、必要かつ適切な監督を行います。
+              </p>
+            </li>
+            <li>
+              <h3>業務委託先、提携先について</h3>
+              <p>
+                当社では、取得した個人データについて、その利用目的の達成のため、取扱いを委託する場合があります。
+                その際は、個人情報を適正に取扱っていると認められるものを選定し、契約等により個人情報保護に必要な事項を義務付け、適切な監督を行います。
+              </p>
+            </li>
+            <li>
+              <h3>法令等の遵守</h3>
+              <p>
+                当社では、個人情報の保護に関する法律、及び個人情報保護に関する法令、ガイドラインを遵守します。<br>
+                また、個人情報保護のために必要な社内規程を定め、遵守するとともに、社会環境の変化に合わせて継続的改善を行います。
+              </p>
+            </li>
+            <li>
+              <h3>個人情報の管理責任者</h3>
+              <p>
+                当社では、個人情報に対するお客様からのお問合せ、ご意見、苦情など、お客様の声を真摯に受け止め、適切かつ迅速に対応します。<br>
+                電話番号：<a href="tel:0538-34-2191">0538-34-2191</a>
+              </p>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section class="footer_breadcrumbs pc">
+        <div class="wrapper">
+          <ul>
+            <li>
+              <a href="/">TOP</a>
+            </li>
+            <li>
+              <a href="/privacy/">プライバシーポリシー</a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
+
+    <footer>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/footer_box.php"); ?>
+    </footer>
+  </div><!-- //AllBox -->
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/footer_tag.php"); ?>
+</body>
+
+</html>
